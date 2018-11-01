b8c489ca30af3005b534ae9af4df1b04de60367e
melissa <melissazhu223@gmail.com>
2018-11-01 11:54:28 +1100

    Updates

---
+++ precache-manifest.8480f7a2af41d32443c3ab78378031a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "803d56717692a7e405cf4518caa6632a",
+    "url": "/newssearch/static/media/background.803d5671.jpg"
+  },
+  {
+    "revision": "2d13fe85c9ad4a68b6ca",
+    "url": "/newssearch/static/js/runtime~main.2d13fe85.js"
+  },
+  {
+    "revision": "103f93933dfa34b59868",
+    "url": "/newssearch/static/js/main.103f9393.chunk.js"
+  },
+  {
+    "revision": "b811a12da3cbf94affa3",
+    "url": "/newssearch/static/js/1.b811a12d.chunk.js"
+  },
+  {
+    "revision": "103f93933dfa34b59868",
+    "url": "/newssearch/static/css/main.500d23ca.chunk.css"
+  },
+  {
+    "revision": "158b91ac8b15b59f73cfc7f15cd95484",
+    "url": "/newssearch/index.html"
+  }
+];
